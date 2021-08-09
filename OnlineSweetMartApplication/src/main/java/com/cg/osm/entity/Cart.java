bc73f71f76833d6c49cb23e7d2b0114d33f2a352
mukuchau <mukuchau@DIN77003626.corp.capgemini.com>
2021-08-09 16:40:58 +0530

    mukul commiting

--- OnlineSweetMartApplication/src/main/java/com/cg/osm/entity/Cart.java
+++ OnlineSweetMartApplication/src/main/java/com/cg/osm/entity/Cart.java
@@ -9,6 +9,36 @@ public class Cart {
 	private int cartId;	
 	private int productCount;
 	private double total;
+	public double getGrandTotal() {
+		return grandTotal;
+	}
+	public void setGrandTotal(double grandTotal) {
+		this.grandTotal = grandTotal;
+	}
+	public List<Product> getListProduct() {
+		return listProduct;
+	}
+	public void setListProduct(List<Product> listProduct) {
+		this.listProduct = listProduct;
+	}
+	public int getCartId() {
+		return cartId;
+	}
+	public void setCartId(int cartId) {
+		this.cartId = cartId;
+	}
+	public int getProductCount() {
+		return productCount;
+	}
+	public void setProductCount(int productCount) {
+		this.productCount = productCount;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
 
 	
 }
