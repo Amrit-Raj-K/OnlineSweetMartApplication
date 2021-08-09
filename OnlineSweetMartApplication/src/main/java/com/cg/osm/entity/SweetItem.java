7e84d70272535c9db8be1b7383091c2bbffda65d
RAJ BHAVAN <RAJ BHAVAN@LAPTOP-JIIJ69T6>
2021-08-09 12:20:33 +0530

    second commit

--- OnlineSweetMartApplication/src/main/java/com/cg/osm/entity/SweetItem.java
+++ OnlineSweetMartApplication/src/main/java/com/cg/osm/entity/SweetItem.java
@@ -1,27 +1,43 @@
 package com.cg.osm.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table
 public class SweetItem {
-    private Integer orderItemId;
-    private Product product;
-    private SweetOrder sweetOrder;
+
+	@Id
+	private Integer orderItemId;
+
+	private Product product;
+	@ManyToOne
+	private SweetOrder sweetOrder;
+
 	public Integer getOrderItemId() {
 		return orderItemId;
 	}
+
 	public void setOrderItemId(Integer orderItemId) {
 		this.orderItemId = orderItemId;
 	}
+
 	public Product getProduct() {
 		return product;
 	}
+
 	public void setProduct(Product product) {
 		this.product = product;
 	}
+
 	public SweetOrder getSweetOrder() {
 		return sweetOrder;
 	}
+
 	public void setSweetOrder(SweetOrder sweetOrder) {
 		this.sweetOrder = sweetOrder;
 	}
-    
+
 }
