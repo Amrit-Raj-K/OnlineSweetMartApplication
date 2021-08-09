327f09dd9b2bbe029712db4cfa636321cb65b80f
RAJ BHAVAN <RAJ BHAVAN@LAPTOP-JIIJ69T6>
2021-08-09 14:39:22 +0530

    repo

--- OnlineSweetMartApplication/src/main/java/com/cg/osm/repository/SweetItemRepository.java
+++ OnlineSweetMartApplication/src/main/java/com/cg/osm/repository/SweetItemRepository.java
@@ -2,10 +2,12 @@ package com.cg.osm.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.cg.osm.entity.SweetItem;
 import com.cg.osm.error.SweetItemNotFoundException;
 
-public interface SweetItemRepository {
+public interface SweetItemRepository extends JpaRepository<SweetItem, Integer>{
 
 	public SweetItem addSweetItem(SweetItem SweetItem);
 	public SweetItem updateSweetItem(SweetItem SweetItem) throws SweetItemNotFoundException;
