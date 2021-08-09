fff0c55bc98ad69673c3c2102a9f54ed19636008
nazia fatima <nazia fatima>
2021-08-09 17:26:24 +0530

    commit nazia

--- OnlineSweetMartApplication/src/main/java/com/cg/osm/entity/Customer.java
+++ OnlineSweetMartApplication/src/main/java/com/cg/osm/entity/Customer.java
@@ -11,4 +11,38 @@ public class Customer {
     private Set<SweetOrder> sweetOrders;
     private List<SweetItem> sweetItems;
 	private Cart cart;
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public Set<SweetOrder> getSweetOrders() {
+		return sweetOrders;
+	}
+	public void setSweetOrders(Set<SweetOrder> sweetOrders) {
+		this.sweetOrders = sweetOrders;
+	}
+	public List<SweetItem> getSweetItems() {
+		return sweetItems;
+	}
+	public void setSweetItems(List<SweetItem> sweetItems) {
+		this.sweetItems = sweetItems;
+	}
+	public Cart getCart() {
+		return cart;
+	}
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
+	
+	
+	
+	
 }
